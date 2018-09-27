3a899e30cd84632c180ba9a097777169d29ccb54
TebionDL <purreren@gmail.com>
2018-09-27 17:45:22 +0200

    añadida función de barra de búsqueda que se abre y cierra

--- scrollTop.js
+++ scrollTop.js
@@ -1,18 +1,45 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $('.goUp').click(function(){
+    $('.goUp').click(function () {
         $('body, html').animate({
             scrollTop: '0px'
         });
     });
 
-    $(window).scroll(function(){
-        if($(this).scrollTop() > 0){
-          $('.goUp').slideDown(300);
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 0) {
+            $('.goUp').slideDown(300);
 
-        }else{
-          $('.goUp').slideUp(300);
+        } else {
+            $('.goUp').slideUp(300);
         }
-     });
+    });
+
+    var x = 0;
+
+    $(".navSearchIcon").click(() => {
+        if (x == 0) {
+            $(".navSearch").show();
+            x = 1;      
+            console.log(x);
+        } else {
+            $(".navSearch").hide();
+            x = 0;
+            console.log(x);
+        }
+
+    });
+
+
+
+    function myFunction() {
+        var x = document.getElementById("myTopnav");
+        if (x.className === "topnav") {
+            x.className += " responsive";
+        } else {
+            x.className = "topnav";
+        }
+    }
+
 
 });
