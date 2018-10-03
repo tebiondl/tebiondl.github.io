5aafd3a525f0f52aab9568607acf41b01a09d9a0
TebionDL <purreren@gmail.com>
2018-10-03 18:13:19 +0200

    arreglado responsive y estrellas

--- web-script.js
+++ web-script.js
@@ -192,9 +192,40 @@ function navAppear(){
 }
 
 
+document.addEventListener("DOMContentLoaded", function(){
+    getStars();
+});
+
+function getStars(){
+loadJSON('https://api.github.com/repos/tebiondl/tebiondl.github.io',
+         function(data) { console.log(data); },
+         function(xhr) { console.error(xhr); }
+   );
+}
 
 
+function loadJSON(path, success, error)
+{
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function()
+    {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                    var myParsed = JSON.parse(xhr.responseText);
+                     document.getElementById("getStars").innerHTML = "Las estrellas que tengo en mi repositorio de github son "+myParsed.stargazers_count;
+            } else {
+                if (error)
+                    error(xhr);
+            }
+        }
+    };
+    xhr.open("GET", path, true);
+    xhr.send();
+}
 
+
+  
 
 
 
